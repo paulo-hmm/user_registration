18c71f87bceaced91fd4b067feb6250dc3f96e03
paulo-hmm <phmarquesmartins@gmail.com>
2024-02-14 13:12:02 -0300

    create database and usuarios.html

--- app_cadastro/views.py
+++ app_cadastro/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .models import Usuario
 
 # Create your views here.
 def home(request):
     return render(request, 'usuarios/home.html') 
 
-def Usuarios(request):
-    return render(request, 'usuarios/home.index.html/')
+def usuarios(request):
+    # Salvar os dados da tela no banco de dados
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    # Mostrar os usuários cadastrados em outra página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    # Retornar os dados para a página de listagem de usuários
+    return render(request, 'usuarios/usuarios.html', usuarios)
